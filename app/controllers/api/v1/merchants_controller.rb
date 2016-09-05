5f6c603bd71903937ceb1bb7ea5617bc79010998
Tommasina <tommasina1@gmail.com>
2016-09-05 16:57:37 -0600

    Adds route and view for merchants' revenue

---
+++ app/controllers/api/v1/merchants_controller.rb
@@ -0,0 +1,5 @@
+class Api::V1::MerchantsController < ApplicationController
+  def show
+    @merchant = Merchant.find(params[:id])
+  end
+end
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,4 +1,17 @@
 class Merchant < ApplicationRecord
   has_many :invoices
   has_many :items
+  
+  def total_revenue(id)
+    # merchant = Merchant.includes(:invoices)    
+    merchant_revenue = [] 
+    merchant = Merchant.find(id)
+    # require "pry"; binding.pry
+    merchant.invoices.each do |invoice|
+     invoice.items.each do |item|
+       merchant_revenue << item.unit_price
+     end
+    end    
+    merchant_revenue.reduce(:+)
+  end
 end
