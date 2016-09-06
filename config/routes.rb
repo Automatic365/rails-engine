ef5e0d64d9303c00e53cb0d93b73518b399de93f
Tommasina <tommasina1@gmail.com>
2016-09-06 17:26:23 -0600

    Adds Items controller and index and show methods and routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
    namespace :api do
      namespace :v1 do
        resources :invoices, only: [:index, :show]
+       resources :items, only: [:index, :show]
        
        namespace :merchants do
          get '/:id/revenue', to: "revenues#show", as: "revenue"
