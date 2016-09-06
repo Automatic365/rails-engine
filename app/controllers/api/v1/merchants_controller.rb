bdc24163030f50bea1f6bc0f535b34efa1e3b281
Jason Hanna <jason.hanna01@gmail.com>
2016-09-06 17:40:54 -0600

    merchants index and show record endpoints

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,6 +1,12 @@
 class Api::V1::MerchantsController < ApplicationController
   respond_to :json
 
+  def index
+    merchant = Merchant.all
+
+    respond_with merchant
+  end
+
   def show
     merchant = Merchant.find(params[:id])
 
