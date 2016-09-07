8d258e5725e37a930d259d6a09c9e94822e5c0a2
Jason Hanna <jason.hanna01@gmail.com>
2016-09-06 21:55:48 -0600

    add random merchant functionality

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :merchants, only:[:index, :show] do
-        get '/:id/revenue', on: :collection, to: "merchants/revenues#show"
-        get '/find', on: :collection, to: "merchants/searches#show"
-        get '/find_all', on: :collection, to: "merchants/searches#index"
+        get ':id/revenue', on: :collection, to: "merchants/revenues#show"
+        get 'find', on: :collection, to: "merchants/searches#show"
+        get 'find_all', on: :collection, to: "merchants/searches#index"
+        get 'random', on: :collection, to: "merchants/random#show"
       end
     end
   end
