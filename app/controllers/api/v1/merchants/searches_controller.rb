d6427b747e27f2c974080a697de5375665f326d3
Jason Hanna <jason.hanna01@gmail.com>
2016-09-06 21:31:44 -0600

    add find and find all routes for merchant

--- app/controllers/api/v1/merchants/searches_controller.rb
+++ app/controllers/api/v1/merchants/searches_controller.rb
@@ -1,16 +1,23 @@
 class Api::V1::Merchants::SearchesController < ApplicationController
   respond_to :json
 
-  def show
-    merchant = Merchant.find_by(request.query_parameters)
-
-    respond_with merchant
-  end
-
   def index
-    merchant = Merchant.where(request.query_parameters)
+    merchant = Merchant.where(merchant_params)
 
     respond_with merchant
   end
+
+  def show
+    merchant = Merchant.find_by(merchant_params)
+
+    respond_with merchant
+  end
+
+  private
+
+  def merchant_params
+    params.permit(:id, :name, :created_at, :updated_at)
+  end
+
 
 end
