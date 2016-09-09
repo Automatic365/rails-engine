29293790bcb89988a6657b040ab700c30adacedb
Tommasina <chompasina@users.noreply.github.com>
2016-09-08 21:09:53 -0600

    Revenue refactor tm (#52)
    * Adds transaction invoices relationship with a show method and route
    
    * Adds customers relationship routes with invoices and transactions
    
    * Adds Item Best Day logic to Items Revenue controller
    
    * Adds revenue by date business logic and several miscellaneous refactors:
    - refactors merchant revenue method
    - formats unit price in items in partials
    -creates transaction-merchant relationship
    - revises rake import task to have unit price formatted as an integer
    
    * Revises merchant revenue by date and merchant total revenue methods and views

--- app/controllers/api/v1/merchants/revenues_controller.rb
+++ app/controllers/api/v1/merchants/revenues_controller.rb
@@ -3,11 +3,16 @@ class Api::V1::Merchants::RevenuesController < ApplicationController
 
   def show
     if params[:date]
-      @merchant = Merchant.revenue_by_day(params[:id], params[:date])
-      respond_with @merchant
+      @date_revenue = Merchant.revenue_by_day(params[:id], params[:date])
+      respond_with @date_revenue
     else
       @merchant = Merchant.find(params[:id])
       respond_with @merchant
     end
   end
+  
+  private
+    def revenue_params
+      params.permit(:id, :date)
+    end
 end
--- app/views/api/v1/merchants/revenues/show.json.jbuilder
+++ app/views/api/v1/merchants/revenues/show.json.jbuilder
@@ -1,1 +1,6 @@
-json.revenue @merchant.total_revenue
+  if params[:date]
+    json.revenue @date_revenue
+  else
+    json.revenue @merchant.total_revenue
+  end
+
